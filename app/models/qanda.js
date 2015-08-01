33ca37fa11e6fc2b3a6ff8afd55b3780e15a3455
ejay3 <aeiou10203@yahoo.com>
2015-08-01 10:57:13 -0500

    hi

--- app/models/qanda.js
+++ app/models/qanda.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var qandaSchema = mongoose.Schema({
 	question: String,
-	anwser: String
+	answer: String
 });
 
 module.exports = mongoose.model('qanda', qandaSchema);
