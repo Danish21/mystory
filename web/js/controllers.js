a7377a9baf3f5683562638d19e0ec3d5c7087330
ejay3 <aeiou10203@yahoo.com>
2015-08-03 17:25:50 -0500

    testing

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -112,15 +112,6 @@ angular.module('appname.controllers',[])
 		});
 	};
 
-	$scope.submitQuestion = function (question) {
-		profileService.updateQuestion(question).then(function (result) {
-			if (result.status === 'OK') {
-				toastr.success('Question submitted');
-				$scope.askquestion = false;
-			}
-		});
-	};
-
 	$scope.askquestion = false;
 
 }]);
