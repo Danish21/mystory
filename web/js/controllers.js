6416d23dc8f6452d63c74ec3965c3b2609cdd1ef
ejay3 <aeiou10203@yahoo.com>
2015-08-04 10:46:24 -0500

    testing

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -114,10 +114,10 @@ angular.module('appname.controllers',[])
 
 	$scope.saveQuestion = function (question) {
 		profileService.submitQuestion(question).then(function (result) {
-			if (result.status === 'OK') {
+			// if (result.status === 'OK') {
 				toastr.success('Question Submitted');
 				$scope.askquestion = false;
-			}
+			// }
 		});
 	};
 
